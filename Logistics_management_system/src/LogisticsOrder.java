260bef3d87ec59be14bbf5d9c93cdcd10a00dc36
zsh <957070524@qq.com>
2024-07-09 15:41:32 +0800

    订单实体类2.0

--- Logistics_management_system/src/LogisticsOrder.java
+++ Logistics_management_system/src/LogisticsOrder.java
@@ -1,26 +1,53 @@
 import java.io.Serializable;
+import java.util.Date;
 
-// 确保 LogisticsOrder 类实现 Serializable 接口
-class LogisticsOrder implements Serializable {
-    private static final long serialVersionUID = 1L; // 建议添加这个字段
+public class LogisticsOrder implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String orderID;
-    private String customer;
+    private String customerName;
     private String address;
-    private java.util.Date orderDate;
-    private double amount;
+    private Date orderDate;
+    private double totalAmount;
     private boolean delivered;
 
-    // 构造方法、getter 和 setter 方法
+    public LogisticsOrder(String orderID, String customerName, String address, Date orderDate, double totalAmount, boolean delivered) {
+        this.orderID = orderID;
+        this.customerName = customerName;
+        this.address = address;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.delivered = delivered;
+    }
+
+    // Getters and Setters
+    public String getOrderID() { return orderID; }
+    public void setOrderID(String orderID) { this.orderID = orderID; }
+
+    public String getCustomerName() { return customerName; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public Date getOrderDate() { return orderDate; }
+    public void setOrderDate(Date orderDate) { this.orderDate = orderDate; }
+
+    public double getTotalAmount() { return totalAmount; }
+    public void setTotalAmount(double totalAmount) { this.totalAmount = totalAmount; }
+
+    public boolean isDelivered() { return delivered; }
+    public void setDelivered(boolean delivered) { this.delivered = delivered; }
 
     @Override
     public String toString() {
         return "物流订单{" +
                 "订单ID='" + orderID + '\'' +
-                ", 客户='" + customer + '\'' +
+                ", 客户='" + customerName + '\'' +
                 ", 地址='" + address + '\'' +
                 ", 订单日期=" + orderDate +
-                ", 金额=" + amount +
+                ", 金额=" + totalAmount +
                 ", 是否送达=" + delivered +
                 '}';
     }
 }
+
