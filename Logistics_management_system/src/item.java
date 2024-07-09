346c47618b8db102f00caf54e2f45a2eb2621f82
white-gardenia613 <2571358956@qq.com>
2024-07-09 15:38:07 +0800

    刘一山第三次提交

---
+++ Logistics_management_system/src/item.java
@@ -0,0 +1,55 @@
+public class item {
+    private String itemID;
+    private String name;
+    private double weight;
+    private double dimensions;
+    private double price;
+
+    public item(String itemID, String name, double weight, double dimensions, double price) {
+        this.itemID = itemID;
+        this.name = name;
+        this.weight = weight;
+        this.dimensions = dimensions;
+        this.price = price;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(double dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
